127aeb9f5f9e52997b5ee9899de78bf5b479bb8c
Alvaro Castaneda <varomix@meta.com>
2023-01-14 00:00:44 -0700

    day 4

--- main.cpp
+++ main.cpp
@@ -1,19 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
 #include <SDL2/SDL.h>
+
+#ifndef MAP_ANONYMOUS
+#define MAP_ANONYMOUS MAP_ANON
+#endif
 
 #define internal static
 #define local_persist static
 #define global_variable static
 
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
 global_variable SDL_Texture *texture;
-global_variable void *pixels;
-global_variable int texture_width;
+global_variable void *BitmapMemory;
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
+global_variable int BytesPerPixel = 4;
+
+internal void
+RenderWeirdGradient(int blue_offset, int green_offset) {
+    int width = BitmapWidth;
+    int height = BitmapHeight;
+
+    int pitch = width * BytesPerPixel;
+    uint8 *row = (uint8 *) BitmapMemory;
+    for (int y = 0; y < BitmapHeight; ++y) {
+        uint32 *pixel = (uint32 *) row;
+        for (int x = 0; x < BitmapWidth; ++x) {
+            uint8 blue = (x + blue_offset);
+            uint8 green = (y + green_offset);
+
+            *pixel++ = ((green << 8) | blue);
+        }
+
+        row += pitch;
+    }
+}
 
 internal void
 SDLResizeTexture(SDL_Renderer *renderer, int width, int height) {
-    if (pixels) {
-        free(pixels);
+    if (BitmapMemory) {
+        munmap(BitmapMemory, BitmapWidth * BitmapHeight * BytesPerPixel);
     }
     if (texture) {
         SDL_DestroyTexture(texture);
@@ -23,13 +60,19 @@ SDLResizeTexture(SDL_Renderer *renderer, int width, int height) {
                                 SDL_TEXTUREACCESS_STREAMING,
                                 width,
                                 height);
-    texture_width = width;
-    pixels = malloc(width * height * 4);
+    BitmapWidth = width;
+    BitmapHeight = height;
+    BitmapMemory = mmap(0, width * height * BytesPerPixel,
+                        PROT_READ | PROT_WRITE,
+                        MAP_PRIVATE | MAP_ANONYMOUS,
+                        -1,
+                        0);
+
 }
 
 internal void
 SDLUpdateWindow(SDL_Window *window, SDL_Renderer *renderer) {
-    SDL_UpdateTexture(texture, 0, pixels, texture_width * 4);
+    SDL_UpdateTexture(texture, 0, BitmapMemory, BitmapWidth * BytesPerPixel);
     SDL_RenderCopy(renderer, texture, 0, 0);
     SDL_RenderPresent(renderer);
 }
@@ -84,12 +127,24 @@ int main() {
         SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
         if (renderer) {
-            for (;;) {
+            bool running = true;
+            int width, height;
+            SDL_GetWindowSize(window, &width, &height);
+            SDLResizeTexture(renderer, width, height);
+            int x_offset = 0;
+            int y_offset = 0;
+            while (running) {
                 SDL_Event event;
-                SDL_WaitEvent(&event);
-                if (HandleEvent(&event)) {
-                    break;
+                while (SDL_PollEvent(&event)) {
+                    if (HandleEvent(&event)) {
+                        running = false;
+                    }
                 }
+                RenderWeirdGradient(x_offset, y_offset);
+                SDLUpdateWindow(window, renderer);
+
+                ++x_offset;
+                y_offset += 2;
             }
         } else {
             // TODO(varomix): Logging
